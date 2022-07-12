7f3dcf11cac4ae143b03b44e122936655fe268dd
Youngwon Son <yoson@c3r2s6.42seoul.kr>
2022-07-12 20:30:47 +0900

    20220712

--- 02_get_next_line/get_next_line.c
+++ 02_get_next_line/get_next_line.c
@@ -6,8 +6,21 @@
 /*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 01:06:48 by yoson             #+#    #+#             */
-/*   Updated: 2022/07/11 01:06:50 by yoson            ###   ########.fr       */
+/*   Updated: 2022/07/12 20:30:21 by yoson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*str;
 
+	while (read(fd, buffer, 1))
+	{
+		if (*buffer == '\n')
+		{
+			str = ft_strdup(buffer);
+		}
+		buffer++;
+	}
+}
