3d9c2dcc58dad20e5e670e9068f528c684e3be83
Youngwon Son <yoson@c2r6s7.42seoul.kr>
2022-07-15 19:57:56 +0900

    필수 파트 완료

--- 02_get_next_line/get_next_line.c
+++ 02_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 01:06:48 by yoson             #+#    #+#             */
-/*   Updated: 2022/07/15 16:52:37 by yoson            ###   ########.fr       */
+/*   Updated: 2022/07/15 19:57:30 by yoson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,21 @@ char	*get_next_line(int fd)
 	size_t			head;
 
 	head = current;
-	while (read(fd, buffer + current, 1) != -1)
+	while (read(fd, &buffer[current], 1))
 	{
 		if (buffer[current] == '\n')
 		{
-			line = gnl_strdup(head);
+			line = gnl_strdup(&buffer[head]);
 			current++;
 			return (line);
 		}
 		current++;
 	}
+	if (!(current - head))
+		return (NULL);
 	line = (char *) malloc(sizeof(char) * (current - head + 1));
 	if (!line)
 		return (NULL);
-	gnl_strlcpy(line, head, current - head + 1);
+	gnl_strlcpy(line, &buffer[head], current - head + 1);
 	return (line);
 }
