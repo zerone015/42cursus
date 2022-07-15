7879e7a1feff76aadd8270f52866a2d238d30516
Youngwon Son <yoson@c2r6s7.42seoul.kr>
2022-07-15 16:52:45 +0900

    20220715

--- 02_get_next_line/get_next_line.c
+++ 02_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 01:06:48 by yoson             #+#    #+#             */
-/*   Updated: 2022/07/14 20:52:57 by yoson            ###   ########.fr       */
+/*   Updated: 2022/07/15 16:52:37 by yoson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	char		*buffer_head;
-	char		*line;
-	size_t		len;
+	char			buffer[BUFFER_SIZE + 1];
+	char			*line;
+	static size_t	current;
+	size_t			head;
 
-	buffer_head = buffer;
-	len = 0;
-	while (read(fd, buffer, 1) != -1)
+	head = current;
+	while (read(fd, buffer + current, 1) != -1)
 	{
-		if (*buffer == '\n')
+		if (buffer[current] == '\n')
 		{
-			line = gnl_strdup(buffer_head);
-			buffer++;
+			line = gnl_strdup(head);
+			current++;
 			return (line);
 		}
-		len++;
-		buffer++;
+		current++;
 	}
-	line = (char *) malloc(sizeof(char) * (len + 1));
+	line = (char *) malloc(sizeof(char) * (current - head + 1));
 	if (!line)
 		return (NULL);
-	gnl_strlcpy(line, buffer_head, len + 1);
+	gnl_strlcpy(line, head, current - head + 1);
 	return (line);
 }
