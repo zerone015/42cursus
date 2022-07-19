55fa43458f81ecf99aae5a1ada913298e3006b7d
zerone015 <zerone015@naver.com>
2022-07-19 13:54:06 +0900

    20220719

---
+++ 03_ft_printf/libft/ft_itoa.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 20:04:52 by yoson             #+#    #+#             */
+/*   Updated: 2022/07/17 23:30:51 by yoson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+static int	get_digit_len(int digit)
+{
+	int	len;
+
+	len = 0;
+	if (digit <= 0)
+		len++;
+	while (digit)
+	{
+		digit /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static long long	get_abs(int digit)
+{
+	long long	tmp;
+
+	tmp = digit;
+	if (digit < 0)
+		tmp *= -1;
+	return (tmp);
+}
+
+static char	*get_digit_str(char *digit_str, int digit, int len)
+{
+	long long	tmp;
+
+	if (!digit)
+	{
+		digit_str[0] = '0';
+		digit_str[1] = '\0';
+		return (digit_str);
+	}
+	tmp = get_abs(digit);
+	digit_str[len--] = '\0';
+	while (tmp)
+	{
+		digit_str[len--] = tmp % 10 + '0';
+		tmp /= 10;
+	}
+	return (digit_str);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*digit_str;
+	int			len;
+
+	len = get_digit_len(n);
+	digit_str = (char *) malloc(sizeof(char) * (len + 1));
+	if (!digit_str)
+		return (NULL);
+	digit_str = get_digit_str(digit_str, n, len);
+	if (n < 0)
+		digit_str[0] = '-';
+	return (digit_str);
+}
---
+++ 03_ft_printf/libft/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 20:09:22 by yoson             #+#    #+#             */
+/*   Updated: 2022/07/12 22:03:31 by yoson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0' && i < size)
+		i++;
+	while (src[j] != '\0' && (i + j + 1) < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	if (i < size)
+		dest[i + j] = '\0';
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	return (i + src_len);
+}
