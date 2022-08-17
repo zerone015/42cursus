511c4445f86f082fe2dfc6d27ebed976db125baa
Youngwon Son <yoson@c6r5s7.42seoul.kr>
2022-08-17 20:32:35 +0900

    연산 구현 완료

---
+++ 07_push_swap/srcs/operation.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operation.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoson <yoson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 19:47:17 by yoson             #+#    #+#             */
+/*   Updated: 2022/08/17 20:31:38 by yoson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap(t_deque *deque, char *str)
+{
+	int	temp;
+
+	temp = deque->head->next->num;
+	deque->head->next->num = deque->head->next->next->num;
+	deque->head->next->next->num = temp;
+	ft_putendl_fd(str, 1);
+}
+
+void	push(t_deque *deque, int num, char *str)
+{
+	add_first(deque, num);
+	ft_putendl_fd(str, 1);
+}
+
+void	rotate(t_deque *deque, char *str)
+{
+	deque->tail->prev->next = deque->head->next;
+	deque->head->next = deque->head->next->next;
+	deque->head->next->prev = deque->head;
+	deque->tail->prev->next->prev = deque->tail->prev;
+	deque->tail->prev->next->next = deque->tail;
+	deque->tail->prev = deque->tail->prev->next;
+	ft_putendl_fd(str, 1);
+}
+
+void	reverse_rotate(t_deque *deque, char *str)
+{
+	deque->head->next->prev = deque->tail->prev;
+	deque->tail->prev = deque->tail->prev->prev;
+	deque->tail->prev->next = deque->tail;
+	deque->head->next->prev->next = deque->head->next;
+	deque->head->next->prev->prev = deque->head;
+	deque->head->next = deque->head->next->prev;
+	ft_putendl_fd(str, 1);
+}
