cfe1a216f3e210025a3d45d2c6b9c425b265a4c1
Youngwon Son <yoson@c6r1s8.42seoul.kr>
2022-12-23 21:22:03 +0900

    Feat element parsing

--- 10_cub3d/includes/cub3d.h
+++ 10_cub3d/includes/cub3d.h
@@ -6,16 +6,20 @@
 /*   By: yoson <yoson@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:05:57 by yoson             #+#    #+#             */
-/*   Updated: 2022/09/21 11:56:17 by yoson            ###   ########.fr       */
+/*   Updated: 2022/12/23 20:47:38 by yoson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define TRUE	1
-# define FALSE	0
-# define ERROR	-1
+# define ERROR				-1
+
+enum e_bool
+{
+	FALSE,
+	TRUE
+};
 
 enum e_direction
 {
@@ -35,7 +39,8 @@ typedef struct s_info
 {
 	char	*texture[4];
 	t_color	color;
-	char	*map;
+	int		resolution[2];
+	char	**map;
 }	t_info;
 
 void	parse_file(t_info *info, const char *filename);
