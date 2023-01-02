12f7eda562d8cee97e27918db7e201f031a687ee
zerone015 <zerone015@naver.com>
2023-01-03 04:29:21 +0900

    cpp01/ex02 finish

---
+++ 12_cpp_module/cpp01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+
+    std::cout << "string memory address: " << &string << "\n";
+    std::cout << "stringPTR memory address: " << stringPTR << "\n";
+    std::cout << "stringREF memory address: " << &stringREF << "\n";
+
+    std::cout << "stringPTR value: " << *stringPTR << "\n";
+    std::cout << "stringREF value: " << stringREF << std::endl;
+    return (0);
+}
