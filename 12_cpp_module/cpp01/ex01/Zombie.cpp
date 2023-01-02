43e9c1db4d5ad49c7211f194ac807692ac1ee016
zerone015 <zerone015@naver.com>
2023-01-03 03:09:48 +0900

    cpp01/ex01 finish

---
+++ 12_cpp_module/cpp01/ex01/Zombie.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Zombie.hpp"
+
+void    Zombie::setName(std::string name)
+{
+    this->name = name;
+}
+
+void    Zombie::announce(void)
+{
+    std::cout << name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << name << " is destroyed" << std::endl;
+}
