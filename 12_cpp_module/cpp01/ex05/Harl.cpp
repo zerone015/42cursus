d3fe1be44ec6a4726fd1cd44791bad636800c0c7
zerone015 <zerone015@naver.com>
2023-01-04 00:26:56 +0900

    Harl message update

--- 12_cpp_module/cpp01/ex05/Harl.cpp
+++ 12_cpp_module/cpp01/ex05/Harl.cpp
@@ -15,21 +15,21 @@ Harl::Harl()
 
 void    Harl::debug()
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << "\n";
     std::cout << "I really do!" << std::endl;
 }
 
 void    Harl::info()
 {
-    std::cout << "I cannot believe adding extra bacon costs more money.";
-    std::cout << "You didn’t put enough bacon in my burger!";
+    std::cout << "I cannot believe adding extra bacon costs more money." << "\n";
+    std::cout << "You didn’t put enough bacon in my burger!" << "\n";
     std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void    Harl::warning()
 {
-    std::cout << "I think I deserve to have some extra bacon for free.";
-    std::cout << " I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << "\n";
+    std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error()
