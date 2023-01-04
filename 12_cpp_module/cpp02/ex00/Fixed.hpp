3823a7afaf1dfc5a2424b025b49690e5be8f8056
zerone015 <zerone015@naver.com>
2023-01-04 20:17:01 +0900

    modify variable name

--- 12_cpp_module/cpp02/ex00/Fixed.hpp
+++ 12_cpp_module/cpp02/ex00/Fixed.hpp
@@ -5,7 +5,7 @@ class Fixed
 {
     private:
         int                 value;
-        static const int    fractional = 8;
+        static const int    bits = 8;
     public:
         int     getRawBits() const;
         void    setRawBits(const int raw);
