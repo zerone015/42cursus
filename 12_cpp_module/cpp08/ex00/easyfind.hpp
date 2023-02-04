7ca4a2bda1695755ee1738163e8cb7585e42982c
zerone015 <zerone015@naver.com>
2023-02-04 09:03:43 +0900

    cpp08/ex00 finish

--- 12_cpp_module/cpp08/ex00/easyfind.hpp
+++ 12_cpp_module/cpp08/ex00/easyfind.hpp
@@ -4,9 +4,12 @@
 # include <algorithm>
 
 template <typename T>
-typename T::iterator    easyfind(const T &container, int value)
+typename T::iterator    easyfind(T &container, int value)
 {
-    return std::find(container.begin(), container.end(), value);
+    typename T::iterator iter = std::find(container.begin(), container.end(), value);
+    if (iter == container.end())
+        throw std::invalid_argument("Invalid Argument");
+    return iter;
 }
 
 #endif
--- 12_cpp_module/cpp08/ex00/main.cpp
+++ 12_cpp_module/cpp08/ex00/main.cpp
@@ -1,13 +1,46 @@
+#include <iostream>
 #include <vector>
 #include <list>
 #include <deque>
-#include <stack>
-#include <queue>
-#include <set>
-#include <map>
+#include "easyfind.hpp"
 
 int main()
 {
-    std::vector<int>
+    try
+    {
+        std::vector<int> vector;
+        vector.push_back(1);
+        vector.push_back(2);
+        vector.push_back(10);
+        std::cout << "vector: " << *easyfind(vector, 10) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "vector: " << e.what() << "\n";
+    }
+    try
+    {
+        std::list<int> list;
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(9);
+        std::cout << "list: " << *easyfind(list, 4) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << "\n";
+    }
+    try
+    {
+        std::deque<int> deque;
+        deque.push_back(1);
+        deque.push_back(2);
+        deque.push_back(3);
+        std::cout << "deque: " << *easyfind(deque, 4) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
     return 0;
 }
