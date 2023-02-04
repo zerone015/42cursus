bc97f84c10887cda32e84b6af8a68caf72e38d24
zerone015 <zerone015@naver.com>
2023-02-04 12:24:30 +0900

    cpp08/ex02 finish

--- 12_cpp_module/cpp08/ex02/MutantStack.hpp
+++ 12_cpp_module/cpp08/ex02/MutantStack.hpp
@@ -12,7 +12,11 @@ class MutantStack : public std::stack<T>
         reverse_iterator rbegin(void) {return this->c.rbegin();}
         reverse_iterator rend(void) {return this->c.rend();}
         typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
+        const_iterator cbegin(void) {return this->c.cbegin();}
+	    const_iterator cend(void) {return this->c.cend();}
         typedef typename MutantStack<T>::stack::container_type::const_reverse_iterator const_reverse_iterator;
+        const_reverse_iterator crbegin(void) {return this->c.crbegin();}
+	    const_reverse_iterator crend(void) {return this->c.crend();}
         MutantStack(){};
         MutantStack(const MutantStack &src) {*this = src;}
         MutantStack& operator=(const MutantStack &src) {this->c = src.c; return *this;}
