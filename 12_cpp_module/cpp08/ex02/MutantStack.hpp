80705772465f9db9a7cc6fe5667b3252a34a44d5
Youngwon Son <yoson@c4r2s4.42seoul.kr>
2023-02-06 22:03:11 +0900

    cpp08/ex02 finish

--- 12_cpp_module/cpp08/ex02/MutantStack.hpp
+++ 12_cpp_module/cpp08/ex02/MutantStack.hpp
@@ -8,15 +8,19 @@ class MutantStack : public std::stack<T>
         typedef typename MutantStack<T>::stack::container_type::iterator iterator;
         iterator begin(void) {return this->c.begin();}
         iterator end(void) {return this->c.end();}
+
         typedef typename MutantStack<T>::stack::container_type::reverse_iterator reverse_iterator;
         reverse_iterator rbegin(void) {return this->c.rbegin();}
         reverse_iterator rend(void) {return this->c.rend();}
+
         typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
         const_iterator cbegin(void) {return this->c.cbegin();}
 	    const_iterator cend(void) {return this->c.cend();}
+
         typedef typename MutantStack<T>::stack::container_type::const_reverse_iterator const_reverse_iterator;
         const_reverse_iterator crbegin(void) {return this->c.crbegin();}
 	    const_reverse_iterator crend(void) {return this->c.crend();}
+        
         MutantStack(){};
         MutantStack(const MutantStack &src) {*this = src;}
         MutantStack& operator=(const MutantStack &src) {this->c = src.c; return *this;}
