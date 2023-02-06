af741ee4acd116849c7beafc616927c44ba5b651
Youngwon Son <yoson@c4r2s4.42seoul.kr>
2023-02-06 21:53:31 +0900

    cpp07/ex01 fix

--- 12_cpp_module/cpp07/ex01/iter.hpp
+++ 12_cpp_module/cpp07/ex01/iter.hpp
@@ -11,7 +11,14 @@ void    iter(T array[], size_t len, void (*f)(T &))
 }
 
 template <typename T>
-void    display(T &a)
+void    iter(const T array[], size_t len, void (*f)(const T &))
+{
+    for (size_t i = 0; i < len; i++)
+        f(array[i]);
+}
+
+template <typename T>
+void    display(const T &a)
 {
     std::cout << a << "\n";
 }
--- 12_cpp_module/cpp07/ex01/main.cpp
+++ 12_cpp_module/cpp07/ex01/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "iter.hpp"
 
 int main()
