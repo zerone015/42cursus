d5748dd4dc28e255a920b31c750eebe1d7576976
zerone015 <zerone015@naver.com>
2023-02-12 18:17:58 +0900

    add lexicographical_compare.hpp

---
+++ 13_ft_containers/lexicographical_compare.hpp
@@ -0,0 +1,36 @@
+#ifndef LEXICOGRAPHICAL_COMPARE_HPP
+# define LEXICOGRAPHICAL_COMPARE_HPP
+
+namespace ft
+{
+    template <typename InputIterator1, typename InputIterator2>  
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+    {
+        while (first1 != last1)
+        {
+            if (first2 == last2 || *first2 < *first1) 
+                return false;
+            else if (*first1 < *first2) 
+                return true;
+            ++first1; 
+            ++first2;
+        }
+        return first2 != last2;
+    }
+    template <typename InputIterator1, typename InputIterator2, typename Compare>  
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+    {
+        while (first1 != last1)
+        {
+            if (first2 == last2 || comp(*first2, *first1)) 
+                return false;
+            else if (comp(*first1, *first2)) 
+                return true;
+            ++first1; 
+            ++first2;
+        }
+        return first2 != last2;
+    }
+}
+
+#endif
