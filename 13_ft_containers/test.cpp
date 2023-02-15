1a3fbc5da6f4facacf0ebba9b6741ecf969aa400
zerone015 <zerone015@naver.com>
2023-02-15 19:23:52 +0900

    add map iterators

--- 13_ft_containers/test.cpp
+++ 13_ft_containers/test.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "map.hpp"
 #include <map>
+#include <vector>
 
 int main()
 {
-    {
-        std::map<char, int> map;
-    }
-    {
-        ft::map<char, int> map;
-    }
+    // std::map<char, int> map;
+    // map.insert(std::pair<char, int>('a', 114));
+    // map.insert(std::pair<char, int>('b', 444));
+
+    // std::map<char, int>::iterator a = map.begin();
+
+    // ++a;
+    // std::map<char, int>::iterator b = map.end();
+    // if (a == b)
+    //     std::cout << "asdf" << std::endl;
+    // std::map<char, int> map2(a, b);
+    // std::cout << map2.size() << std::endl;
+
 }
