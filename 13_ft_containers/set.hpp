86f7eabccafaee2e9613a3caad8f04656e991c23
zerone015 <zerone015@naver.com>
2023-02-18 03:04:08 +0900

    create set.hpp

---
+++ 13_ft_containers/set.hpp
@@ -0,0 +1,13 @@
+#ifndef SET_HPP
+# define SET_HPP
+
+namespace ft
+{
+    template <class Key, class Compare = std::less<Key>, class Allocator = std::allocator<Key> > 
+    class set
+    {
+
+    }
+}
+
+#endif
