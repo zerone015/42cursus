544564535535cb9974221d685625adcaf0f39ac4
YoungWon Son <zerone015@naver.com>
2023-02-28 16:07:02 +0900

    inception 필수파트 완료

--- 14_inception/srcs/requirements/mariadb/conf/create_db.sh
+++ 14_inception/srcs/requirements/mariadb/conf/create_db.sh
@@ -5,6 +5,11 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         chown -R mysql:mysql /var/lib/mysql
 
         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+	
+	tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+                return 1
+        fi
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
